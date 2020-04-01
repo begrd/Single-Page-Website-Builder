d43940df169aea81a261484a8ab408a08d571b85
begrd <martynas.belogradovas73@gmail.com>
2020-04-01 15:58:04 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = 'localhost';
-$duombazesUser = 'root';
-$duombazesPass = '';
+$servername = 'fojvtycq53b2f2kx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+$duombazesUser = 'mrod23azo9cv3a3s';
+$duombazesPass = 'u1wk2esf1mzcnv00';
 $db = 'portfis';
 $conn = mysqli_connect($servername, $duombazesUser, $duombazesPass, $db); 
 
